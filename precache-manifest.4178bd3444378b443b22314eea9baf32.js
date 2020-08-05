769c5b1ecb43601290125036f484b76c461695d1
Praveen D <1pdpraveen@gmail.com>
2020-08-05 21:50:40 +0530

    Updates

---
+++ precache-manifest.4178bd3444378b443b22314eea9baf32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e2533a72e5a087a84f5bc63259b5dd",
+    "url": "/TEST/index.html"
+  },
+  {
+    "revision": "f1f096ced64dfbdb3859",
+    "url": "/TEST/static/css/main.1590806d.chunk.css"
+  },
+  {
+    "revision": "cadb83d287e325f91aec",
+    "url": "/TEST/static/js/2.fa1425df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TEST/static/js/2.fa1425df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f096ced64dfbdb3859",
+    "url": "/TEST/static/js/main.3e239b57.chunk.js"
+  },
+  {
+    "revision": "776d7b0cd8e740e3392a",
+    "url": "/TEST/static/js/runtime-main.e3517cb1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/TEST/static/media/SEGA.12a3db8b.woff"
+  }
+]);
